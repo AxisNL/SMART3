54975e82cc96d18c8d416a51dba6edbcff974673
Angelo Hongens <support@telco-mgmt.nl>
2026-03-25 16:30:55 -0400

    init

--- services/web/app/api/admin.py
+++ services/web/app/api/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Fileserver, Folder
+
+from .models import Fileserver, PhysicalMountPoint, VirtualMountPoint, OrgFolder, PermissionFolder, Permission, User, \
+    Role, RoleMembership
 
 # Register your models here.
 admin.site.register(Fileserver)
-admin.site.register(Folder)
+admin.site.register(PhysicalMountPoint)
+admin.site.register(VirtualMountPoint)
+admin.site.register(OrgFolder)
+admin.site.register(PermissionFolder)
+admin.site.register(Permission)
+admin.site.register(User)
+admin.site.register(Role)
+admin.site.register(RoleMembership)
--- services/web/app/api/models.py
+++ services/web/app/api/models.py
@@ -5,30 +5,118 @@ class Fileserver(models.Model):
     name = models.CharField(
         max_length=256
     )
+
+    def __str__(self):
+        return self.name
+
+
+class PhysicalMountPoint(models.Model):
+    """
+        This is the physical path on a file server that hosts a fileshare
+        """
     physicalprefix = models.CharField(
         max_length=30,
         help_text="A prefix on the fileserver, like D:\Shares")
+    fileserver = models.ForeignKey('Fileserver', null=True, blank=True, related_name='child', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.physicalprefix} on {self.fileserver}"
+
+
+class VirtualMountPoint(models.Model):
     logicalprefix = models.CharField(
         max_length=30,
         help_text="A logical prefix the user sees, like O:"
     )
 
     def __str__(self):
-        return self.name
+        return f"{self.logicalprefix}"
 
 
-class Folder(models.Model):
+class OrgFolder(models.Model):
     name = models.CharField(
         max_length=256
     )
-    parent_folder = models.ForeignKey('self', null=True, blank=True, related_name='child', on_delete=models.CASCADE)
-    fileserver = models.ForeignKey('Fileserver', null=True, blank=True, related_name='child', on_delete=models.CASCADE)
+    physical_mountpoint = models.ForeignKey('PhysicalMountPoint',
+                                            null=True,
+                                            blank=True,
+                                            on_delete=models.CASCADE)
+    virtual_mountpoint = models.ForeignKey('VirtualMountPoint',
+                                           null=True,
+                                           blank=True,
+                                           on_delete=models.CASCADE)
+    parent_folder = models.ForeignKey('self',
+                                      null=True,
+                                      blank=True,
+                                      related_name='child',
+                                      on_delete=models.CASCADE)
 
     def __str__(self):
-        if self.fileserver is not None:
+        if self.virtual_mountpoint is not None:
             # this is a root folder
-            return  f"{self.fileserver.logicalprefix}\\{self.name}"
+            return f"{self.virtual_mountpoint.logicalprefix}\\{self.name}"
         if self.parent_folder is not None:
             return f"{self.parent_folder}\\{self.name}"
         return None
+
+
+class PermissionFolder(models.Model):
+    name = models.CharField(max_length=256)
+
+    parent_folder = models.ForeignKey('OrgFolder',
+                                      null=True,
+                                      blank=True,
+                                      on_delete=models.CASCADE)
+    owner = models.ForeignKey('User',
+                              null=True,
+                              blank=True,
+                              on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.parent_folder}\\{self.name}"
+
+
+class Permission(models.Model):
+    role = models.ForeignKey('Role', on_delete=models.CASCADE)
+    folder = models.ForeignKey('PermissionFolder', on_delete=models.CASCADE)
+
+    READONLY = "R"
+    READWRITE = "W"
+
+    PERMISSION_CHOICES = {
+        READONLY: "ReadOnly",
+        READWRITE: "ReadWrite"
+    }
+    permission = models.CharField(
+        max_length=1,
+        choices=PERMISSION_CHOICES,
+        default=READWRITE,
+    )
+
+    def __str__(self):
+        return f"{self.folder}\\{self.role}"
+
+
+class User(models.Model):
+    name = models.CharField(max_length=256)
+    sid = models.CharField(max_length=256, blank=True, null=True)
+    email = models.CharField(max_length=256, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=512, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class RoleMembership(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    role = models.ForeignKey('Role', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} role {self.role}"
